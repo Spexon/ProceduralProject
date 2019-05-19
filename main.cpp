fa9086809eb7da4819a2995a8a5cea76b1adeaa1
Spexon <46728234+Spexon@users.noreply.github.com>
2019-05-19 13:04:20 -0400

    Added comments

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    cout << "Hello There! Please select an option below:\n"; //Main Menu
+    cout << "Production Line Tracker\n\n";
+    cout << "1. Add Employee Account\n" << "2. Add Music Player\n" << "3. Add Movie Player\n"
+         << "4. Display Production Statistics\n" << "5. Exit\n";
+
+    cout << "Please enter your username: \n"; //Username for employers to log in (incomplete)
+    string user;
+    getline(cin, user);
+
+    cout << "Please enter your password: \n"; //Password for employers to log in
+    string pass;
+    getline(cin, pass);
+
+    return 0;
+
+}
+
